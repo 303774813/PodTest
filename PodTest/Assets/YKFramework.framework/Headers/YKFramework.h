974b7c74a4ee3b8eac145a5e3cb9018d739f9d36
mima.789 <wangchao7@yiche.com>
2021-01-05 17:33:44 +0800

    d

---
+++ PodTest/Assets/YKFramework.framework/Headers/YKFramework.h
@@ -0,0 +1,18 @@
+//
+//  YKFramework.h
+//  YKFramework
+//
+//  Created by wangchao on 2021/1/5.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YKFramework.
+FOUNDATION_EXPORT double YKFrameworkVersionNumber;
+
+//! Project version string for YKFramework.
+FOUNDATION_EXPORT const unsigned char YKFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YKFramework/PublicHeader.h>
+
+
---
+++ PodTest/Assets/YKFramework.framework/Headers/YKTool.h
@@ -0,0 +1,16 @@
+//
+//  YKTool.h
+//  YKFramework
+//
+//  Created by wangchao on 2021/1/5.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YKTool : NSObject
++ (void)test;
+@end
+
+NS_ASSUME_NONNULL_END
